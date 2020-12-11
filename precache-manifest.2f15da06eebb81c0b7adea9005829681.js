da0438cfa860f5dcc0a32d617bc37fe106c1eac6
Ankit Pathak <ankitpathak143192@gmail.com>
2020-12-11 21:15:58 +0530

    Updates

---
+++ precache-manifest.2f15da06eebb81c0b7adea9005829681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd04f3325821cb633ddcb650a593ed0",
+    "url": "/inginnv1/index.html"
+  },
+  {
+    "revision": "be51fffc83c212532f93",
+    "url": "/inginnv1/static/css/main.e6eba4d3.chunk.css"
+  },
+  {
+    "revision": "fb6e6296f7f1524f3319",
+    "url": "/inginnv1/static/js/2.8a4b947c.chunk.js"
+  },
+  {
+    "revision": "c897be67765481464f9e500ed505cd42",
+    "url": "/inginnv1/static/js/2.8a4b947c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be51fffc83c212532f93",
+    "url": "/inginnv1/static/js/main.c0eaaaf1.chunk.js"
+  },
+  {
+    "revision": "aae430125dbc0cae21f3",
+    "url": "/inginnv1/static/js/runtime-main.5588da24.js"
+  }
+]);
